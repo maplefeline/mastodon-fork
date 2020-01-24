1e15755779dead78e5c399b8692ef4d0ddd30783
Asa Aveanna <asa@holomaplefeline.net>
2020-01-23 19:12:17 -0800

    Assign a reporting user.

--- app/models/account.rb
+++ app/models/account.rb
@@ -497,7 +497,13 @@ class Account < ApplicationRecord
   def preempt_domain_block
     if Account.find_by(domain: domain).nil? && DomainBlock.find_by(domain: domain).nil?
       Rails.logger.info "Premptivly blocking #{username}@#{domain}"
-      ReportService.new.call(nil, self, comment: 'Quarantined new domain')
+      admin = Account.joins(:user).merge(User.staff).first
+      ReportService.new.call(
+        admin,
+        self,
+        comment: 'Quarantined new domain',
+        forward: false,
+      )
       DomainBlock.new(domain: domain, severity: :suspend, reject_media: true, reject_reports: true).save!
     end
   end
